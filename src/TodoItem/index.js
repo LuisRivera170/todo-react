11799667dc47c157906e38234046d6f9813382b0
Luis Rivera <luis.rivera@infotec.mx>
2021-11-01 22:23:23 -0600

    Modifying styles

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -8,7 +8,7 @@ export function TodoItem({text, completed, onComplete, onDelete}) {
     return (
        <li className="TodoItem">
            <CompleteIcon completed={ completed } onComplete={ onComplete } />
-           <p className={`TodoItem.p ${completed && 'TodoItem-p--completed'}`}>{text}</p>
+           <p className={`TodoItem-p ${completed && 'TodoItem-p--completed'}`}>{text}</p>
            <DeleteIcon onDelete={ onDelete } />
        </li>
     )
